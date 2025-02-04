cf1c7d12f9ca0cda2383360ce41a7e7e284c0e0f
Yat <61630043+Yatloaf@users.noreply.github.com>
2025-02-04 21:13:35 +0100

    Fix onPlayerRemove not invalidating tab list cache

--- src/main/java/dev/yatloaf/modkrowd/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/dev/yatloaf/modkrowd/mixin/ClientPlayNetworkHandlerMixin.java
@@ -3,6 +3,7 @@ package dev.yatloaf.modkrowd.mixin;
 import dev.yatloaf.modkrowd.ModKrowd;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket;
+import net.minecraft.network.packet.s2c.play.PlayerRemoveS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,6 +11,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
+	@Inject(method = "onPlayerRemove", at = @At("RETURN"))
+	public void onPlayerRemoveInject(PlayerRemoveS2CPacket packet, CallbackInfo ci) {
+		ModKrowd.invalidateTabListCache();
+	}
+
 	@Inject(method = "onPlayerList", at = @At("RETURN"))
 	public void onPlayerListInject(PlayerListS2CPacket packet, CallbackInfo ci) {
 		ModKrowd.invalidateTabListCache();
