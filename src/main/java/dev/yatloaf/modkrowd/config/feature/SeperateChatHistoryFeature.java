6a55b05e16b79d6d6736fbccdcfcb7757e050f7c
Yat <61630043+Yatloaf@users.noreply.github.com>
2025-02-05 21:29:01 +0100

    Rework Separate Chat History

--- src/main/java/dev/yatloaf/modkrowd/config/feature/SeperateChatHistoryFeature.java
+++ src/main/java/dev/yatloaf/modkrowd/config/feature/SeperateChatHistoryFeature.java
@@ -11,26 +11,24 @@ import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.ServerInfo;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class SeperateChatHistoryFeature extends Feature {
+    private static final ChatHud.ChatState EMPTY_CHAT_STATE = new ChatHud.ChatState(List.of(), List.of(), List.of());
 
-    private final Map<String, ChatHud.ChatState> chatStates;
-    private String currentAddress = null;
+    private final Map<Location, ChatHud.ChatState> chatStates = new HashMap<>();
+    private Location currentLocation = null;
 
     public SeperateChatHistoryFeature(String id, PredicateIndex allowedPredicates) {
         super(id, allowedPredicates);
-        this.chatStates = new HashMap<>();
     }
 
     @Override
     public void onEnable(MinecraftClient client, ActionQueue queue) {
         ClientPlayNetworkHandler handler = client.getNetworkHandler();
         if (handler != null) {
-            ServerInfo info = handler.getServerInfo();
-            if (info != null) {
-                this.chatStates.put(info.address, client.inGameHud.getChatHud().toChatState());
-            }
+            this.chatStates.put(Location.of(handler.getServerInfo()), client.inGameHud.getChatHud().toChatState());
         }
     }
 
@@ -41,25 +39,43 @@ public class SeperateChatHistoryFeature extends Feature {
 
     @Override
     public void onJoin(ClientPlayNetworkHandler handler, MinecraftClient client, ActionQueue queue) {
-        ServerInfo info = handler.getServerInfo();
-        if (info != null) {
-            if (!info.address.equals(this.currentAddress)) {
-                this.currentAddress = info.address;
-                if (this.chatStates.containsKey(info.address)) {
-                    client.inGameHud.getChatHud().restoreChatState(this.chatStates.get(info.address));
-                }
-                client.inGameHud.getChatHud().addMessage(ModKrowd.CONFIG.themeCustom(new SelfAlohaMessage(Aloha.JOIN, info)).text());
+        Location location = Location.of(handler.getServerInfo());
+        if (!location.equals(this.currentLocation)) {
+            this.currentLocation = location;
+            client.inGameHud.getChatHud().restoreChatState(this.chatStates.getOrDefault(location, EMPTY_CHAT_STATE));
+            if (location instanceof Server server) {
+                client.inGameHud.getChatHud().addMessage(ModKrowd.CONFIG.themeCustom(new SelfAlohaMessage(Aloha.JOIN, server.info)).text());
             }
         }
     }
 
     @Override
     public void onDisconnect(ClientPlayNetworkHandler handler, MinecraftClient client, ActionQueue queue) {
-        ServerInfo info = handler.getServerInfo();
-        if (info != null) {
-            client.inGameHud.getChatHud().addMessage(ModKrowd.CONFIG.themeCustom(new SelfAlohaMessage(Aloha.LEAVE, info)).text());
-            this.chatStates.put(info.address, client.inGameHud.getChatHud().toChatState());
+        Location location = Location.of(handler.getServerInfo());
+        if (location instanceof Server server) {
+            client.inGameHud.getChatHud().addMessage(ModKrowd.CONFIG.themeCustom(new SelfAlohaMessage(Aloha.LEAVE, server.info)).text());
         }
-        this.currentAddress = null;
+        this.chatStates.put(location, client.inGameHud.getChatHud().toChatState());
+        this.currentLocation = null;
+    }
+
+    private interface Location {
+        Location ELSEWHERE = new Location() {};
+
+        private static Location of(ServerInfo info) {
+            return info == null ? ELSEWHERE : new Server(info);
+        }
+    }
+
+    private record Server(ServerInfo info) implements Location {
+        @Override
+        public boolean equals(Object obj) {
+            return obj instanceof Server server && this.info.address.equals(server.info.address);
+        }
+
+        @Override
+        public int hashCode() {
+            return this.info.address.hashCode();
+        }
     }
 }
