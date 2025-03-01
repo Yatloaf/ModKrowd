8afd85ec7cc968037a7d543219c10ffb097d184a
Yat <61630043+Yatloaf@users.noreply.github.com>
2025-03-01 15:05:15 +0100

    Fix Message Preview mishandling Direction.LOOP

--- src/main/java/dev/yatloaf/modkrowd/config/feature/ClickResponseFeature.java
+++ src/main/java/dev/yatloaf/modkrowd/config/feature/ClickResponseFeature.java
@@ -2,7 +2,6 @@ package dev.yatloaf.modkrowd.config.feature;
 
 import dev.yatloaf.modkrowd.config.PredicateIndex;
 import dev.yatloaf.modkrowd.config.queue.ActionQueue;
-import dev.yatloaf.modkrowd.cubekrowd.common.SelfPlayer;
 import dev.yatloaf.modkrowd.cubekrowd.common.cache.TextCache;
 import dev.yatloaf.modkrowd.cubekrowd.message.DirectMessage;
 import dev.yatloaf.modkrowd.cubekrowd.message.cache.CubeKrowdMessageCache;
@@ -20,17 +19,9 @@ public class ClickResponseFeature extends Feature {
         if (message instanceof CubeKrowdMessageCache ckMessage) {
             DirectMessage dm = ckMessage.directMessageFast();
             if (dm.isReal()) {
-                switch (dm.direction()) {
-                    case INCOMING -> message.setThemed(TextCache.of(ckMessage.original.text().copy().styled(style -> style.withClickEvent(
-                            new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/msg " + dm.sender() + " ")
-                    ))));
-                    case OUTGOING -> message.setThemed(TextCache.of(ckMessage.original.text().copy().styled(style -> style.withClickEvent(
-                            new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/msg " + dm.recipient() + " ")
-                    ))));
-                    case LOOP -> message.setThemed(TextCache.of(ckMessage.original.text().copy().styled(style -> style.withClickEvent(
-                            new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/msg " + SelfPlayer.username() + " ")
-                    ))));
-                }
+                message.setThemed(TextCache.of(ckMessage.original.text().copy().styled(style -> style.withClickEvent(
+                        new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/msg " + dm.other() + " ")
+                ))));
             }
         }
     }
