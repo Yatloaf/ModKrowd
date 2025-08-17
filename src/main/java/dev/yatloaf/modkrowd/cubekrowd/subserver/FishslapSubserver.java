8fedfe26eb95c992ce2b290500c3e8d82a8ad4e7
Yat <61630043+Yatloaf@users.noreply.github.com>
2025-08-18 01:00:57 +0200

    Fix Message Preview not including angled brackets in BuildTeam

--- src/main/java/dev/yatloaf/modkrowd/cubekrowd/subserver/FishslapSubserver.java
+++ src/main/java/dev/yatloaf/modkrowd/cubekrowd/subserver/FishslapSubserver.java
@@ -3,8 +3,6 @@ package dev.yatloaf.modkrowd.cubekrowd.subserver;
 import dev.yatloaf.modkrowd.cubekrowd.common.CKColor;
 import dev.yatloaf.modkrowd.cubekrowd.common.MinigameTeam;
 import dev.yatloaf.modkrowd.cubekrowd.common.cache.TextCache;
-import dev.yatloaf.modkrowd.cubekrowd.common.SelfPlayer;
-import dev.yatloaf.modkrowd.util.text.StyledString;
 
 public class FishslapSubserver extends MinigameSubserver {
     public FishslapSubserver(String id, String listName, String... tabNames) {
@@ -18,15 +16,6 @@ public class FishslapSubserver extends MinigameSubserver {
 
     @Override
     public TextCache formatChat(String message) {
-        StyledString prefix = SelfPlayer.rankNameSoft().appearance();
-        if (StyledString.EMPTY.equals(prefix)) {
-            return TextCache.EMPTY;
-        } else {
-            return TextCache.of(StyledString.concat(
-                    StyledString.fromString("<"),
-                    prefix,
-                    StyledString.fromString("> " + message)
-            ));
-        }
+        return Subserver.formatChatMixed(message);
     }
 }
